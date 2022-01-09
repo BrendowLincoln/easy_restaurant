d8d4dd71c15bd3bc27547e419fb031b6e6c18baa
BrendowLincoln <linconlbrendow@gmail.com>
2022-01-09 19:34:31 -0300

    Main - Estruturando o projeto no formato Modular

---
+++ lib/app/modules/home/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'home_store.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  const HomePage({Key? key, this.title = "Home"}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends ModularState<HomePage, HomeStore> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Observer(
+              builder: (context) => Text(
+                '${store.counter}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          store.increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
