cb29fa90468a1ebf65bb00613b59a756554153a0
BrendowLincoln <linconlbrendow@gmail.com>
2022-01-21 20:59:29 -0300

    Main - Criando a pagina de login

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -1,15 +1,14 @@
-import 'package:easy_restaurant/app/modules/auth/auth_Page.dart';
 import 'package:easy_restaurant/app/modules/auth/auth_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'login/login_page.dart';
+
 class AuthModule extends Module {
   @override
-  final List<Bind> binds = [
-    Bind.lazySingleton((i) => AuthStore()),
-  ];
+  final List<Bind> binds = [Bind.lazySingleton((i) => AuthStore())];
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute('/', child: (_, args) => AuthPage()),
+    ChildRoute('/', child: (_, args) => LoginPage()),
   ];
 }
---
+++ lib/app/modules/auth/login/login_page.dart
@@ -0,0 +1,98 @@
+import 'package:easy_restaurant/app/modules/auth/auth_store.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class LoginPage extends StatefulWidget {
+  final store = Modular.get<AuthStore>();
+
+  LoginPage({Key? key}) : super(key: key);
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.96,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/images/logo.png',
+                    width: MediaQuery.of(context).size.width * 0.8,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 20.0, horizontal: 10.0),
+                            hintText: 'Usuário',
+                            hintStyle: TextStyle(color: Colors.white),
+                            focusColor: Theme.of(context).colorScheme.primary,
+                            filled: true,
+                            fillColor: Colors.white54.withOpacity(0.2),
+                            border: UnderlineInputBorder(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(5.0),
+                                    topRight: Radius.circular(5.0))),
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 20.0, horizontal: 10.0),
+                            hintText: 'Senha',
+                            hintStyle: TextStyle(color: Colors.white),
+                            focusColor: Theme.of(context).colorScheme.secondary,
+                            filled: true,
+                            fillColor: Colors.white54.withOpacity(0.2),
+                            border: UnderlineInputBorder(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(4.0),
+                                    topRight: Radius.circular(4.0))),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            GestureDetector(
+                              child: Text('Esqueceu sua senha?'),
+                              onTap: () {},
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10.0),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          height: 50,
+                          child: ElevatedButton(
+                            child: Text(
+                              'Login',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            onPressed: () => Modular.to.navigate('/home/'),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
